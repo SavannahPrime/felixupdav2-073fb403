93ce899761a2a2a8127f511954c3b8bd3a839801
FREDRICK SARUNI <129012511+L1QU3D@users.noreply.github.com>
2025-04-16 12:26:23 +0000

    updates

--- src/CustomMain.tsx
+++ src/CustomMain.tsx
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import CustomApp from './CustomApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CustomApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
